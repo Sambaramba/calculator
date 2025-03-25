3b16f1e349f6befff0f4de83e041dcb950ef9500
Sambaramba <samuelharrison19@tutanota.com>
2025-03-25 09:11:02 +0000

    feat: add maths functions and variables for maths operations

---
+++ calculator.js
@@ -0,0 +1,25 @@
+//FOUR BASIC MATHS OPERATIONS
+
+function add (a, b ) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b ) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+//VARIABLES FOR THE MATHS EXPRESSION
+
+let numberOne;
+let operator;
+let numberTwo;
+
